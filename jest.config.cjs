a0f4112fe964e4bceaaa28701cd3a5ae47257af6
eduarda-santanna <eduarda.santanna@sap.com>
2025-03-11 21:15:01 -0300

    TOMO-5: unit tests

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.node.json' }],
+  },
+};
